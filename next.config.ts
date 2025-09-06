d89d8a9a4839976f6e31643f96f154e91c4e7041
Rok2496 <rokon@example.com>
2025-09-06 06:17:08 +0600

    Add Render backend to Next.js TypeScript configuration for image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/static/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nasimsir.onrender.com',
+        port: '',
+        pathname: '/static/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
